43e4bc94f130d9eb1d30fdb7d843b3d6bf730225
Ian Maguire <ian.maguire@mariadb.com>
2019-01-23 16:43:02 -0400

    Adjusting var check

--- openshift/entrypoint.sh
+++ openshift/entrypoint.sh
@@ -3,10 +3,13 @@ set -e
 
 /etc/init.d/mysql start
 
-if [ ! -z "${MARIADB_ROOT_PW}" ]
+
+if [[ -n $MARIADB_ROOT_PW ]]
 then
-	mysqladmin -u root password ${MARIADB_ROOT_PW}
+	mysqladmin -u root password $MARIADB_ROOT_PW
 fi
+
+sleep 3
 
 /etc/init.d/mysql restart
 
